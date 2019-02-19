7a720b9ee85be4e7878b6ace0536422b37115da6
Steven Levine <slevine111@gmail.com>
2019-02-19 00:55:06 -0500

    testing syncing with heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const dbInit = require('./DataAccess/index')
 
 const PORT = process.env.PORT || 3000
-const syncForceValue = process.env.FORCE === 'false' ? false : true
+const syncForceValue = !process.env.FORCE === 'false'
 
 dbInit(syncForceValue).then(() => {
   app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
